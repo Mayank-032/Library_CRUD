8d54c8ea9ebe09984eb17f85c37190c809080388
Mayank-032 <mayankmohan74@gmail.com>
2022-07-28 23:18:07 +0530

    Transaction Database functionality completed

--- server.js
+++ server.js
@@ -64,3 +64,50 @@ app.post('/Task2', (req, res) => {
         res.send(result);
     })
 })
+
+// Store Data In Transactions and also update database when book is issued
+app.post('/Task3', async (req, res) => {
+    // console.log(req.body.BookName3)
+    if(req.body.issue !== undefined){
+        if(await db.collection('books')
+        .count({book_name: new RegExp(req.body.BookName3, 'i')}) > 0){
+            
+            if(await db.collection('Transaction')
+            .count({$and:[{book_name: new RegExp(req.body.BookName3, 'i')}, {person_name: new RegExp(req.body.PersonName, 'i')}]}) === 0){
+                
+                await db.collection('Transaction')
+                .insertOne({book_name: req.body.BookName3, person_name: req.body.PersonName, issueDate: new Date()})
+            
+                res.send("Book Issued Successfully");
+            }else{
+                res.send("Book Already Issued");
+            }
+        }else{
+            res.send("No Data Found");
+        }
+    }else{
+        if(await db.collection('Transaction')
+        .countDocuments({$and:[{book_name: req.body.BookName3}, {person_name: req.body.PersonName}]}) > 0){
+            
+            await db.collection('Transaction')
+            .findOneAndUpdate({book_name: req.body.BookName3, person_name: req.body.PersonName}, {$set: {returnDate: new Date()}})
+        
+            let rentPerDay = await db.collection('books')
+            .findOne({book_name: new RegExp(req.body.BookName3, 'i')}, {person_name: new RegExp(req.body.PersonName, 'i')})
+            
+            let dates = await db.collection('Transaction')
+            .findOne({book_name: req.body.BookName3, person_name: req.body.PersonName})
+
+            let date1 = dates.issueDate;
+            let date2 = dates.returnDate;
+            let diff = date1.getTime() - date2.getTime();
+            
+            let TotalDays = Math.ceil(diff / (1000 * 3600 * 24));
+            let rpd = rentPerDay.rent_per_day;
+
+            res.send("Book Returned Successfully!!! Total Rent Calculated: " + ((TotalDays+1)*rpd));
+        }else{
+            res.send("Please Issue The Book First!!!");
+        }
+    }
+})
